d082090b5159a9c72ad3b0869b90e62b6683e1e1
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 10:07:15 +0200

    Update Constants.cs

--- BroMaker/src/BroMakerLib/Constants.cs
+++ BroMaker/src/BroMakerLib/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string NAME = nameof(BroMakerLib);
         public const string AUTHOR = "Gorzontrok";
-        public const string VERSION = "2.0.2";
+        public const string VERSION = "2.1.0";
     }
 }
