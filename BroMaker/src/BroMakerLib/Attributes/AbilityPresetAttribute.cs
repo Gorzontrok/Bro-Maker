60bad3fe8e38e3dd96dafc88820489a0ed32b8fc
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 10:07:09 +0200

    Create AbilityPresetAttribute.cs

---
+++ BroMaker/src/BroMakerLib/Attributes/AbilityPresetAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BroMakerLib.Attributes
+{
+    public class AbilityPresetAttribute : CustomObjectPresetAttribute
+    {
+        public AbilityPresetAttribute(string name) : base(name)
+        { }
+    }
+}
