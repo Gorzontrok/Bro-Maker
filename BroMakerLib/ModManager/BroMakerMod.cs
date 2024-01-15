fcace514ed8b552af48e5d1b03ac9f17dff431ff
Gorzon38 <64686176+Gorzontrok@users.noreply.github.com>
2024-01-15 13:41:24 +0100

    Update BroMakerMod.cs

--- BroMakerLib/ModManager/BroMakerMod.cs
+++ BroMakerLib/ModManager/BroMakerMod.cs
@@ -1,30 +1,27 @@
 ﻿using BroMakerLib.Loggers;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using System;
 using System.IO;
 using System.Linq;
-using System.Net;
-using UnityModManagerNet;
-using static UnityModManagerNet.UnityModManager.Param;
-using UnityEngine;
 
 namespace BroMakerLib
 {
     public class BroMakerMod
     {
-        [JsonProperty(Required = Required.Always)]
-        public string Version;
-        public string BroMakerVersion;
-        public string Name;
-        public string Author;
+        public string Name = string.Empty;
+        public string Version = "0.0.0";
+        public string BroMakerVersion = "0.0.0";
+        public string Author = string.Empty;
         // The custom objects
-        public string[] Assemblies;
-        public string[] CustomBros;
-        public string[] Abilities;
+
+        public string[] Assemblies = new string[0];
+        public string[] CustomBros = new string[0];
+        public string[] Abilities = new string[0];
 
         // Not in JSON file
+        [JsonIgnore]
         public string Path { get; protected set; }
+        [JsonIgnore]
         public string[] BrosNames { get; protected set; }
 
         public static BroMakerMod TryLoad(string path)
@@ -41,9 +38,29 @@ namespace BroMakerLib
 
         public void Initialize()
         {
+            if (Name.IsNullOrEmpty())
+            {
+                Name = Path.Substring(Directory.GetCurrentDirectory().Length - 1, Path.Length);
+            }
             CustomBros = CheckFiles(CustomBros);
             Abilities = CheckFiles(Abilities);
             BrosNames = CustomBros.Select(str => System.IO.Path.GetFileNameWithoutExtension(str)).ToArray();
+        }
+
+
+        public virtual string SerializeJSON(string folderPath, string fileName)
+        {
+            Name = fileName;
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            var json = JsonConvert.SerializeObject(this, Formatting.Indented, settings);
+            File.WriteAllText(System.IO.Path.Combine(folderPath, fileName + ".mod.json"), json);
+            return json;
         }
 
         private string[] CheckFiles(string[] files)
