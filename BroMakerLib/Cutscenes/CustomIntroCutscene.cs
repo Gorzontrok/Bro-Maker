8392adfb5d6779862182013f1eb304453469a6e7
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2024-07-24 19:42:23 +0200

    Update CustomIntroCutscene.cs
    Added Colors for texts
    Added sprite animation
    Rearrange options

--- BroMakerLib/Cutscenes/CustomIntroCutscene.cs
+++ BroMakerLib/Cutscenes/CustomIntroCutscene.cs
@@ -4,7 +4,6 @@ using RocketLib.JsonConverters;
 using Newtonsoft.Json;
 using System;
 using System.IO;
-using System.Collections.Generic;
 using TFBGames.Systems;
 using UnityEngine;
 
@@ -13,29 +12,57 @@ namespace BroMakerLib.Cutscenes
     [Serializable]
     public class CustomIntroCutscene
     {
+        public static readonly Color HeadingDefaultColor = new Color(0.925f, 0.016f, 0.082f, 1.0f);
+        public static readonly string Subtitle1DefaultText = "Joins the Battle!";
+        public static readonly Color SubtitleDefaultColor = new Color(0.278f, 0.376f, 0.953f, 1.0f);
+
         [JsonIgnore, EditorIgnore]
         public static CutsceneIntroData rambroCutscene, requestedCutscene;
 
         [JsonIgnore]
         public string path = string.Empty;
 
-        public string heading = "???";
-        public string subtitle1 = "Joins the Battle!";
-        public string subtitle2 = string.Empty; // This won't be shown unless we use a cutscene with an existing mesh for this or create our own mesh
-        public float headingScale = 0.15f;
-        public float subtitleScale = 0.10f;
         public bool playCutsceneOnFirstSpawn = false;
+
+        // Heading
+        public string heading = "Brononymous";
+        public float headingScale = 0.15f;
+        [JsonConverter(typeof(ColorJsonConverter))]
+        public Color headingColor = HeadingDefaultColor;
+
+        // Subtitle 1
+        public string subtitle1 = Subtitle1DefaultText;
+        [JsonConverter(typeof(ColorJsonConverter))]
+        public Color subtitle1Color = SubtitleDefaultColor;
+
+        // Subtitle 2
+        public bool hasSecondSubtitle = false;
+        public string subtitle2 = string.Empty; // This won't be shown unless we use a cutscene with an existing mesh for this or create our own mesh
+        [JsonConverter(typeof(ColorJsonConverter))]
+        public Color subtitle2Color = SubtitleDefaultColor;
+        public float subtitleScale = 0.10f;
+
+        // Sprite
         public string spritePath = string.Empty;
         [JsonConverter(typeof(Vector2Converter))]
         public Vector2 spriteSize = Vector2.zero;
         [JsonConverter(typeof(RectConverter))]
         public Rect spriteRect = Rect.zero;
-        [JsonConverter(typeof(Vector3Converter))]
+
+        // Sprite Animation
+        public bool isAnimated = false;
+        public float animationStartDelay = 0.0f;
+        public bool animationPingPong = false;
+        public bool animationLoop = false;
+        [JsonConverter(typeof(CutsceneSpriteAnimatedJsonConverter))]
         public Vector3 spriteAnimRateFramesWidth = Vector3.zero;
+
         public string barkPath = string.Empty;
+        public bool playDefaultFanfare = true;
         public string fanfarePath = string.Empty;
         public string anim = string.Empty;
-        public bool playDefaultFanfare = true;
+
+        public string backgroundPath = string.Empty;
 
         // To Implement
         //public AnimationClip animClip; // I don't think i can implement it. Maybe swaping between the existing animation clips.
@@ -66,10 +93,10 @@ namespace BroMakerLib.Cutscenes
                 rambroCutscene = requestedCutscene;
             }
 
-            var data = new CutsceneIntroData();
+            var data = new CutsceneIntroDataExtra();
             data.heading = heading;
             data.subtitle1 = subtitle1;
-            data.subtitle2 = subtitle2;
+            data.subtitle2 = hasSecondSubtitle ? subtitle2 : string.Empty;
             data.headingScale = headingScale;
             data.subtitleScale = subtitleScale;
 
@@ -137,6 +164,28 @@ namespace BroMakerLib.Cutscenes
                 {
                     BMLogger.Log(ex);
                     data.introFanfare = null;
+                }
+            }
+
+            // CutsceneIntroDataExtra
+            data.headingColor = headingColor;
+            data.subtitle1Color = subtitle1Color;
+            data.subtitle2Color = subtitle2Color;
+            data.isAnimated = isAnimated;
+            data.animationStartDelay = animationStartDelay;
+            data.animationPingPong = animationPingPong;
+            data.animationLoop = animationLoop;
+
+            if (!string.IsNullOrEmpty(backgroundPath))
+            {
+                // Load default sprite when the Json doesn't provide one
+                try
+                {
+                    data.backgroundTexture = ResourcesController.GetTexture(path, backgroundPath);
+                }
+                catch (Exception ex)
+                {
+                    BMLogger.Log(ex);
                 }
             }
 
