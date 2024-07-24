ff5fc31378373bec3dcad798dcaffc967a22e6e8
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2024-07-24 19:42:43 +0200

    Created CutsceneIntroDataExtra.cs

---
+++ BroMakerLib/Cutscenes/CutsceneIntroExtra.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BroMakerLib.Cutscenes
+{
+    public class CutsceneIntroDataExtra : CutsceneIntroData
+    {
+        public Color headingColor = Color.black;
+        public Color subtitle1Color = Color.black;
+        public Color subtitle2Color = Color.black;
+
+        public bool isAnimated = false;
+        public float animationStartDelay = 0.0f;
+        public bool animationPingPong = false;
+        public bool animationLoop = false;
+        public Texture2D backgroundTexture = null;
+    }
+}
